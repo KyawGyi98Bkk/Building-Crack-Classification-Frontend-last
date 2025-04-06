0ee45bc4852d14d406c37120f3affd91310ec016
Kyaw Htet Oo <kyawhtetoo1998@gmail.com>
2025-04-06 17:42:38 +0700

    Save changes before rebase

--- src/App.js
+++ src/App.js
@@ -23,27 +23,3 @@ function App() {
 
 export default App;
 
-import React, { useState } from 'react';
-import ImageUpload from './components/ImageUpload';
-import ResultDisplay from './components/ResultDisplay';
-import './App.css';
-
-function App() {
-  const [prediction, setPrediction] = useState(null);
-
-  // Handle the prediction result
-  const handlePrediction = (data) => {
-    setPrediction(data);
-  };
-
-  return (
-    <div className="App">
-      <h1>Building Crack Classification</h1>
-      <ImageUpload onPrediction={handlePrediction} />
-      {prediction && <ResultDisplay prediction={prediction} />}
-    </div>
-  );
-}
-
-export default App;
-
